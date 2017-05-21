6ae68cc57b2854c677850c7787626270dfe1f468
Tony Zhang <tonyzhang617@users.noreply.github.com>
2017-05-20 21:21:28 -0400

    Update BodyEditorLoader.java

--- core/src/com/tianyi/zhang/BodyEditorLoader.java
+++ core/src/com/tianyi/zhang/BodyEditorLoader.java
@@ -13,27 +13,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Loads the collision fixtures defined with the Physics Body Editor
- * application. You only need to give it a body and the corresponding fixture
- * name, and it will attach these fixtures to your body.
- *
- * @author Aurelien Ribon | http://www.aurelienribon.com
- */
 public class BodyEditorLoader {
 
-	// Model
 	private final Model model;
-
-	// Reusable stuff
+	
 	private final List<Vector2> vectorPool = new ArrayList<Vector2>();
 	private final PolygonShape polygonShape = new PolygonShape();
 	private final CircleShape circleShape = new CircleShape();
 	private final Vector2 vec = new Vector2();
-
-	// -------------------------------------------------------------------------
-	// Ctors
-	// -------------------------------------------------------------------------
 
 	public BodyEditorLoader(FileHandle file) {
 		if (file == null) throw new NullPointerException("file is null");
@@ -45,34 +32,6 @@ public class BodyEditorLoader {
 		model = readJson(str);
 	}
 
-	// -------------------------------------------------------------------------
-	// Public API
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Creates and applies the fixtures defined in the editor. The name
-	 * parameter is used to retrieve the right fixture from the loaded file.
-	 * <br/><br/>
-	 *
-	 * The body reference point (the red cross in the tool) is by default
-	 * located at the bottom left corner of the image. This reference point
-	 * will be put right over the BodyDef position point. Therefore, you should
-	 * place this reference point carefully to let you place your body in your
-	 * world easily with its BodyDef.position point. Note that to draw an image
-	 * at the position of your body, you will need to know this reference point
-	 * (see {@link #getOrigin(java.lang.String, float)}.
-	 * <br/><br/>
-	 *
-	 * Also, saved shapes are normalized. As shown in the tool, the width of
-	 * the image is considered to be always 1 meter. Thus, you need to provide
-	 * a scale factor so the polygons get resized according to your needs (not
-	 * every body is 1 meter large in your game, I guess).
-	 *
-	 * @param body The Box2d body you want to attach the fixture to.
-	 * @param name The name of the fixture you want to load.
-	 * @param fd The fixture parameters to apply to the created body fixture.
-	 * @param scale The desired scale of the body. The default width is 1.
-	 */
 	public void attachFixture(Body body, String name, FixtureDef fd, float scale) {
 		RigidBodyModel rbModel = model.rigidBodies.get(name);
 		if (rbModel == null) throw new RuntimeException("Name '" + name + "' was not found.");
@@ -110,10 +69,7 @@ public class BodyEditorLoader {
 			free(center);
 		}
 	}
-
-	/**
-	 * Gets the image path attached to the given name.
-	 */
+	
 	public String getImagePath(String name) {
 		RigidBodyModel rbModel = model.rigidBodies.get(name);
 		if (rbModel == null) throw new RuntimeException("Name '" + name + "' was not found.");
@@ -121,12 +77,6 @@ public class BodyEditorLoader {
 		return rbModel.imagePath;
 	}
 
-	/**
-	 * Gets the origin point attached to the given name. Since the point is
-	 * normalized in [0,1] coordinates, it needs to be scaled to your body
-	 * size. Warning: this method returns the same Vector2 object each time, so
-	 * copy it if you need it for later use.
-	 */
 	public Vector2 getOrigin(String name, float scale) {
 		RigidBodyModel rbModel = model.rigidBodies.get(name);
 		if (rbModel == null) throw new RuntimeException("Name '" + name + "' was not found.");
@@ -134,18 +84,9 @@ public class BodyEditorLoader {
 		return vec.set(rbModel.origin).scl(scale);
 	}
 
-	/**
-	 * <b>For advanced users only.</b> Lets you access the internal model of
-	 * this loader and modify it. Be aware that any modification is permanent
-	 * and that you should really know what you are doing.
-	 */
 	public Model getInternalModel() {
 		return model;
 	}
-
-	// -------------------------------------------------------------------------
-	// Json Models
-	// -------------------------------------------------------------------------
 
 	public static class Model {
 		public final Map<String, RigidBodyModel> rigidBodies = new HashMap<String, RigidBodyModel>();
@@ -168,10 +109,6 @@ public class BodyEditorLoader {
 		public final Vector2 center = new Vector2();
 		public float radius;
 	}
-
-	// -------------------------------------------------------------------------
-	// Json reading process
-	// -------------------------------------------------------------------------
 
 	/*private Model readJson(String str) {
 		Model m = new Model();
@@ -243,8 +180,6 @@ public class BodyEditorLoader {
 			polygon.buffer = new Vector2[polygon.vertices.size()];
 		} */
 
-		// circles
-
 		JsonValue circlesElem = bodyElem.get("circles");
 		
 		for (JsonValue circle : circlesElem) {
@@ -315,10 +250,6 @@ public class BodyEditorLoader {
 
 		return rbModel;
 	} */
-
-	// -------------------------------------------------------------------------
-	// Helpers
-	// -------------------------------------------------------------------------
 
 	private Vector2 newVec() {
 		return vectorPool.isEmpty() ? new Vector2() : vectorPool.remove(0);
